ecab730d373877afc392771f48b1dfcc91cf0de6
2sungryul <67367753+2sungryul@users.noreply.github.com>
2024-05-04 11:35:52 +0900

    Add files via upload

---
+++ src/sub.cpp
@@ -0,0 +1,40 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/compressed_image.hpp"
+#include "opencv2/opencv.hpp"
+#include <memory>
+#include <functional>
+#include <iostream>
+using std::placeholders::_1;
+
+std::string dst = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! \
+	nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! \
+	h264parse ! rtph264pay pt=96 ! \
+	udpsink host = 125.139.250.214 port = 8001 sync=false";
+
+cv::VideoWriter writer;
+cv::Mat frame;
+        
+void mysub_callback(rclcpp::Node::SharedPtr node, const sensor_msgs::msg::CompressedImage::SharedPtr msg)
+{
+    std::cout << "callback" << std::endl;
+    frame = cv::imdecode(cv::Mat(msg->data),  cv::IMREAD_COLOR);
+    writer << frame;
+    RCLCPP_INFO(node->get_logger(), "Received Image : %s,%d,%d", msg->format.c_str(),frame.rows,frame.cols);
+}
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("camsub");
+
+    writer.open(dst, 0, (double)30, cv::Size(640, 360), true);
+    if(!writer.isOpened()) { RCLCPP_ERROR(node->get_logger(), "Writer open failed!"); rclcpp::shutdown(); return -1; }
+
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+    std::function<void(const sensor_msgs::msg::CompressedImage::SharedPtr msg)> fn;
+    fn = std::bind(mysub_callback, node, _1);
+    auto mysub = node->create_subscription<sensor_msgs::msg::CompressedImage>("image/compressed",qos_profile,fn);
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
+
